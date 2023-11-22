c7dce264157201c165472b8989eeb34822917979
emurmudu <eliasmurmudu@gmail.com>
2023-11-22 15:39:59 +0600

    stayVista project created

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to StayVista</h1>
+      {/* categories section  */}
+      {/* room section */}
+    </div>
+  )
+}
+
+export default Home
